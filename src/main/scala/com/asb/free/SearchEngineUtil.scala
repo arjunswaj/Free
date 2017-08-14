66b37cff78e941441846922d4e0b351cf43dad7c
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-08-14 17:32:49 +0530

    Adding Search Engine Utility

---
+++ src/main/scala/com/asb/free/SearchEngineUtil.scala
@@ -0,0 +1,50 @@
+package com.asb.free
+
+import cats.{Id, InjectK, ~>}
+import cats.data.EitherK
+import cats.free.Free
+import cats.free.Free.inject
+import com.asb.free.search.engine.GoogleTools.{GoogleInterpreter, GoogleUtil, GoogleUtils}
+import com.asb.free.search.engine.MicrosoftTools.{MicrosoftInterpreter, MicrosoftUtil, MicrosoftUtils}
+import org.apache.commons.csv.CSVRecord
+
+object SearchEngineUtil {
+
+  case class SEResult(bing: Either[Exception, String], google: Either[Exception, String])
+
+  sealed trait SearchTools[A]
+
+  case class DoSearch(csvRecord: CSVRecord) extends SearchTools[SEResult]
+
+  class SearchEngineRecordProcessors[F[_]](implicit I: InjectK[SearchTools, F]) {
+
+    def processRecord(csvRecord: CSVRecord): Free[F, SEResult] =
+      inject[SearchTools, F](DoSearch(csvRecord))
+
+  }
+
+  object SearchEngineRecordProcessors {
+    def apply[F[_]](implicit I: InjectK[SearchTools, F]): SearchEngineRecordProcessors[F] = new SearchEngineRecordProcessors[F]
+  }
+
+  type GoogleOrMicrosoft[A] = EitherK[GoogleUtil, MicrosoftUtil, A]
+  val interpreter: GoogleOrMicrosoft ~> Id = GoogleInterpreter or MicrosoftInterpreter
+
+  def program(implicit G: GoogleUtils[GoogleOrMicrosoft], M: MicrosoftUtils[GoogleOrMicrosoft], csvRecord: CSVRecord):
+  Free[Free[GoogleOrMicrosoft, SEResult] = {
+    import G._
+    import M._
+    for {
+      goog <- searchGoogle(csvRecord)
+      micr <- searchMicrosoft(csvRecord)
+    } yield SEResult(goog, micr)
+  }
+
+  object SearchEngineInterpreter extends (SearchTools ~> Id) {
+    override def apply[A](fa: SearchTools[A]): Id[A] = fa match {
+      case DoSearch(csvRecord) => program(GoogleUtils[GoogleOrMicrosoft],
+        MicrosoftUtils[GoogleOrMicrosoft], csvRecord).foldMap(interpreter)
+    }
+  }
+
+}
