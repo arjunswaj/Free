c3c3073c419202003dbb00a54463c3d8f259f9ee
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2017-08-14 17:32:01 +0530

    Adding Google Search Engine Tools

---
+++ src/main/scala/com/asb/free/search/engine/GoogleTools.scala
@@ -0,0 +1,31 @@
+package com.asb.free.search.engine
+
+import cats.{Id, InjectK, ~>}
+import cats.free.Free
+import cats.free.Free.inject
+import org.apache.commons.csv.CSVRecord
+
+object GoogleTools {
+
+  sealed trait GoogleUtil[A]
+
+  case class SearchGoogle(csvRecord: CSVRecord) extends GoogleUtil[Either[Exception, String]]
+
+  class GoogleUtils[F[_]](implicit I: InjectK[GoogleUtil, F]) {
+
+    def searchGoogle(csvRecord: CSVRecord): Free[F, Either[Exception, String]] =
+      inject(SearchGoogle(csvRecord))
+
+  }
+
+  object GoogleUtils {
+    def apply[F[_]](implicit I: InjectK[GoogleUtil, F]): GoogleUtils[F] = new GoogleUtils[F]
+  }
+
+  object GoogleInterpreter extends (GoogleUtil ~> Id) {
+    override def apply[A](fa: GoogleUtil[A]): Id[A] = fa match {
+      case SearchGoogle(csvRecord) => Right(csvRecord.get(0))
+    }
+  }
+
+}
